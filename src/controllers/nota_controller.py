30824b0ef75cdc2461b62d33eee5ae8fd08780a9
svictordavid795-wq <svictordavid795@gmail.com>
2026-09-25 18:15:34 -0500

    feat: creación y consulta de notas

--- src/controllers/nota_controller.py
+++ src/controllers/nota_controller.py
@@ -1,2 +1,35 @@
+from flask import request, jsonify
+from models.nota import Nota
+from database import db
+
 def listar_notas():
-    return "Listado de notas desde el controlador"
+    notas = Nota.query.all()
+
+    resultado = []
+
+    for nota in notas:
+        resultado.append({
+            "id": nota.id,
+            "estudiante": nota.estudiante,
+            "asignatura": nota.asignatura,
+            "periodo": nota.periodo,
+            "calificacion": nota.calificacion
+        })
+
+    return jsonify(resultado)
+
+
+def crear_nota():
+    datos = request.get_json()
+
+    nueva_nota = Nota(
+        estudiante=datos["estudiante"],
+        asignatura=datos["asignatura"],
+        periodo=datos["periodo"],
+        calificacion=datos["calificacion"]
+    )
+
+    db.session.add(nueva_nota)
+    db.session.commit()
+
+    return jsonify({"mensaje": "Nota creada correctamente"})
--- src/routes/nota_routes.py
+++ src/routes/nota_routes.py
@@ -1,8 +1,13 @@
 from flask import Blueprint
-from controllers.nota_controller import listar_notas
+from controllers.nota_controller import listar_notas, crear_nota
 
 nota_bp = Blueprint("nota_bp", __name__)
 
-@nota_bp.route("/notas")
+@nota_bp.route("/notas", methods=["GET"])
 def notas():
     return listar_notas()
+
+
+@nota_bp.route("/notas", methods=["POST"])
+def guardar_nota():
+    return crear_nota()
