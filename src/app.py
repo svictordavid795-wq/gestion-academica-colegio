b60004d74d7bbec76a57d7bc4a5a58b38ceb9631
svictordavid795-wq <svictordavid795@gmail.com>
2026-09-25 17:13:42 -0500

    feat: conexión de flask con sqlite

--- src/app.py
+++ src/app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from database import db
 
 app = Flask(__name__)
 
-@app.route('/')
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///gestion_academica.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+
+@app.route("/")
 def inicio():
     return "Sistema de Gestion Academica"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
